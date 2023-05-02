85631146ac16bc080bd5310c38e717d5d2417639
Mohit Pandey <pandeymohit215@gmail.com>
2023-05-02 22:24:41 +0530

    framer

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -4,23 +4,6 @@ import { AnimatePresence, LazyMotion, domAnimation,motion } from 'framer-motion'
 
 export default function App({ Component, pageProps,router }) {
   return <Layout>
-    <LazyMotion features={domAnimation}>
-    <AnimatePresence 
-      mode="wait" 
-      initial={false}
-      onExitComplete={() => window.scrollTo(0, 0)}
-    >
-      <motion.div
-           
-            className="page-wrap"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-          >
-            <Component {...pageProps} key={router.asPath} />
-          </motion.div>
-      
-    </AnimatePresence>
-    </LazyMotion>
-  </Layout>
+            <Component {...pageProps} key={router.asPath} /> 
+          </Layout>
 }
--- client/src/pages/components/Layout.js
+++ client/src/pages/components/Layout.js
@@ -6,18 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 import { motion } from "framer-motion";
 
 const Layout = ({ children }) => (
-  <motion.div
-    initial={{ x: 300, opacity: 0 }}
-    animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 300, opacity: 0 }}
-    transition={{
-      type: "spring",
-      stiffness: 260,
-      damping: 20,
-    }}
-  >
+  <>
     <Navbar />
    <main className={`min-h-screen ${inter.className}`}>{children}</main>
-  </motion.div>
+  </>
 );
 export default Layout;
