eace890dd15ffa0dfeb2054e24f965623e406147
Mohit Pandey <pandeymohit215@gmail.com>
2023-06-18 16:35:22 +0530

    Set up Socket Connection

--- client/src/pages/videoChat.js
+++ client/src/pages/videoChat.js
@@ -2,9 +2,11 @@ import React from 'react'
 import VideoPlayer from './components/classroom/VideoPlayer'
 import Options from './components/classroom/Options'
 import Notifications from './components/classroom/Notifications'
+import { ContextProvider } from '@/SocketContext'
 
 const videoChat = () => {
   return (
+    <ContextProvider>
     <div>
       <h1 className='text-center mt-2 text-xl bg-gray-500 text-white'>Video Classroom</h1>
       <VideoPlayer/>
@@ -12,6 +14,7 @@ const videoChat = () => {
         <Notifications/>
       </Options>
     </div>
+    </ContextProvider>
   )
 }
 
